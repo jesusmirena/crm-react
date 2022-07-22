40ff7b278446b55046ede4f41c373a6ccdb3d3cd
jesusmirena <jesusamh16@gmail.com>
2022-07-21 23:27:19 -0500

    Grouping Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./layout/Layout";
-import Login from "./layout/Login";
+import EditClient from "./pages/EditClient";
+
 import Home from "./pages/Home";
-import LoginForm from "./pages/LoginForm";
+import NewClient from "./pages/NewClient";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />}>
-          <Route index element={<LoginForm />} />
-        </Route>
-
         <Route path="/clients" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path="new" element={<NewClient />} />
+          <Route path="edit/:id" element={<EditClient />} />
         </Route>
       </Routes>
     </BrowserRouter>
